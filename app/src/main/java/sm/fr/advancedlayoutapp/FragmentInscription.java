025628a216b03ecf04ad7321d0e4ae65c82ffe61
Robert <robert@gmail.com>
2018-01-16 13:52:27 +0100

    Ajout d'un user

--- app/src/main/java/sm/fr/advancedlayoutapp/FragmentInscription.java
+++ app/src/main/java/sm/fr/advancedlayoutapp/FragmentInscription.java
@@ -7,12 +7,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FragmentInscription extends Fragment {
+
+    DrawerActivity parentActivity;
+    EditText userNameEditText;
 
     //private android.app.Fragment myself = this;
 
@@ -25,7 +29,32 @@ public class FragmentInscription extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_inscription, container, false);
+        View view =  inflater.inflate(R.layout.fragment_fragment_inscription, container, false);
+
+        // récupération d'une référence au champs du formulaire
+        userNameEditText = view.findViewById(R.id.EditTextUserName);
+        String userName = userNameEditText.getText().toString();
+
+        //Récupération d'une référence à l'activité
+        parentActivity = (DrawerActivity) getActivity();
+
+        //Gestion du clic sur le bouton valider
+        Button btValid = view.findViewById(R.id.btValidateInscription);
+        btValid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Récupération de la saisie de l'utilisateur
+                String userName = userNameEditText.getText().toString();
+                //Récupération de l'entité utilisateur et modification du nom de l'utilisateur
+                parentActivity.getUser().setUserName(userName);
+
+                //Navigation vers le Fragment B en passant par l'activité parent
+                parentActivity.goToFragmentB();
+            }
+        });
+
+        return view;
 
     }
 
