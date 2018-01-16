ac71572297721bae9ee589849086dc9b67c0cfc3
Robert <robert@gmail.com>
2018-01-16 11:10:35 +0100

    Ajout d'une option d'inscription au menu

--- app/src/main/java/sm/fr/advancedlayoutapp/DrawerActivity.java
+++ app/src/main/java/sm/fr/advancedlayoutapp/DrawerActivity.java
@@ -1,5 +1,6 @@
 package sm.fr.advancedlayoutapp;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -71,17 +72,13 @@ public class DrawerActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
+        if (id == R.id.nav_page1) {
+            navigateToFragment(new FragmentB());
+        } else if (id == R.id.nav_inscription) {
+            navigateToFragment(new FragmentInscription());
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
@@ -89,4 +86,14 @@ public class DrawerActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    // Affichage du fragment passé en argument à la place du composant
+    // identifié comme FragmentContainer
+    private void navigateToFragment(Fragment targetFragment){
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmentContainer, targetFragment)
+                .commit();
+    }
+
 }
